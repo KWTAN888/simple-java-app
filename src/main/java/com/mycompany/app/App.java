ea53d1bf135942aa62a148261c4b3b70f7599d1d
KWTAN888 <khangwei8888@gmail.com>
2024-02-20 16:33:42 +0800

    My forth change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkin is Crap !");
+        System.out.println("Jenkin is Crap tool !");
     }
 
     public String getMessage() {
