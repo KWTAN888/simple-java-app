127534f2b1e9b6842be9800a8e03115c043e2886
KWTAN888 <khangwei8888@gmail.com>
2024-02-20 15:24:30 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkin is Awesome");
+        System.out.println("Jenkin is Crap !");
     }
 
     public String getMessage() {
