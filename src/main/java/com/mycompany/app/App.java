481c0a10e89b55f6099b0c044c50179843aeead4
KWTAN888 <khangwei8888@gmail.com>
2024-02-21 09:25:35 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkin is Crap tool !");
+        System.out.println("Testing out webhook");
     }
 
     public String getMessage() {
