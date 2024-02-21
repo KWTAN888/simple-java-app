e3c27057bad8b8631bc4decfcdca4ab926ac074a
KWTAN888 <khangwei8888@gmail.com>
2024-02-21 11:02:54 +0800

    Testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Testing out webhook");
+        System.out.println("Testing out webhook test");
     }
 
     public String getMessage() {
